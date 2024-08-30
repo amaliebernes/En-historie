94f698cc736a53c1815de284cc483c7e5b6173b3
amaliebernes <bernesamalie@gmail.com>
2024-08-30 10:39:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# En-historie
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/amaliebernes/En-historie)
---
+++ script.js
@@ -0,0 +1,31 @@
+console.log('Hello!');
+
+let start = 'Det var en gang';
+
+console.log(start);
+
+// Dette er en kommentar
+
+let a = 10;
+
+let b = 20;
+
+let c = 30;
+
+console.log(a);
+
+let lisa = {
+  name: 'lisa',
+  age: 100,
+  alive: false,
+};
+
+console.log(lisa);
+
+console.log(lisa.alive);
+
+if (lisa.alive) {
+  console.log('hun er i live');
+} else {
+  console.log('hun er et gjenferd');
+}
