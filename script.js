091f5ec55d5791f365e113d652c4b19864fe1cd5
amaliebernes <bernesamalie@gmail.com>
2024-08-30 11:02:07 +0200

    Kommentarer

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ let start = 'Det var en gang';
 
 console.log(start);
 
-// En beskrivelse av en karakter
+start = "En sann historie"
+
+console.log (start)
+
+// let (beskriver noe, console lager noe, =  forandrer noe)
 
 let a = 10
 
@@ -12,6 +16,7 @@ let b = 12
 
 let c = a + b
 
+// (setter inn tallrekker)
 
 console.log(c);
 
@@ -21,12 +26,20 @@ let lisa = {
   alive: false,
 };
 
+//....
+
 console.log(lisa);
 
 console.log(lisa.alive);
+
+// (velger ut noe)
 
 if (lisa.alive) {
   console.log('hun er i live');
 } else {
   console.log('hun er et gjenferd');
 }
+
+
+//(varierende alternativer-sant/ikke sant)
+
