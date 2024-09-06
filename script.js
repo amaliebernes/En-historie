945b3cbf753b444197f62535c7a7dd2022b9965d
amaliebernes <bernesamalie@gmail.com>
2024-09-06 12:30:08 +0200

    Start-mellomrom

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-
-
-let start = 'Det var en gang';
+let start = "Det var en gang";
 
 console.log(start);
 
-start = "En sann historie"
+start = "En sann historie";
 
-console.log (start)
+console.log(start);
 
 // let (beskriver noe, console lager noe, =  forandrer noe)
 
-let a = 10
+let a = 10;
 
-let b = 12
+let b = 12;
 
-let c = a + b
+let c = a + b;
 
 // (setter inn tallrekker)
 
 console.log(c);
 
 let lisa = {
-  name: 'lisa',
+  name: "lisa",
   age: 100,
   alive: false,
 };
@@ -35,11 +33,9 @@ console.log(lisa.alive);
 // (velger ut noe)
 
 if (lisa.alive) {
-  console.log('hun er i live');
+  console.log("hun er i live");
 } else {
-  console.log('hun er et gjenferd');
+  console.log("hun er et gjenferd");
 }
 
-
 //(varierende alternativer-sant/ikke sant)
-
